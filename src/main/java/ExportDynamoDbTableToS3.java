360396e1ebe228847a42bf7e2ceb967ba045c28a
TWpower <elld8850@naver.com>
2021-08-05 16:18:37 +0900

    Initial Commits
    Signed-off-by: TWpower <elld8850@naver.com>

---
+++ src/main/java/ExportDynamoDbTableToS3.java
@@ -0,0 +1,44 @@
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.ExportTableToPointInTimeRequest;
+import software.amazon.awssdk.services.dynamodb.model.ExportTableToPointInTimeRequest;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+public class ExportDynamoDbTableToS3 {
+    public static void main(String[] args) {
+
+        // Please fill out below values
+        String tableArn = "";
+        String s3Bucket = "";
+        String s3Prefix = "";
+        String exportFormat = "";
+        String s3BucketOwner = "";
+
+        // Use one of versions in below
+
+        // Java 1.x
+        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+        ExportTableToPointInTimeRequest exportTableToPointInTimeRequest = new ExportTableToPointInTimeRequest();
+
+        exportTableToPointInTimeRequest.setTableArn(tableArn);
+        exportTableToPointInTimeRequest.setS3Bucket(s3Bucket);
+        exportTableToPointInTimeRequest.setS3Prefix(s3Prefix);
+        exportTableToPointInTimeRequest.setExportFormat(exportFormat);
+        exportTableToPointInTimeRequest.setS3BucketOwner(s3BucketOwner);
+
+        amazonDynamoDB.exportTableToPointInTime(exportTableToPointInTimeRequest);
+
+        // Java 2.x
+        DynamoDbClient dynamoDbClient = DynamoDbClient.builder().build();
+        ExportTableToPointInTimeRequest exportTableToPointInTimeRequest = ExportTableToPointInTimeRequest
+                .builder()
+                .tableArn(tableArn)
+                .s3Bucket(s3Bucket)
+                .s3Prefix(s3Prefix)
+                .exportFormat(exportFormat)
+                .s3BucketOwner(s3BucketOwner)
+                .build();
+
+        dynamoDbClient.exportTableToPointInTime(exportTableToPointInTimeRequest);
+    }
+}
